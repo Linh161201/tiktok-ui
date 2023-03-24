398bbb9e187e18c8d809eb62926e67970072efef
Linh <Redmercy161201@gmail.com>
2023-03-24 15:00:27 +0700

    PropTypes Library

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
-
+import PropTypes from 'prop-types';
 import styles from './Menu.module.scss';
 import Header from './Header';
 import MenuItem from './MenuItem';
@@ -45,7 +45,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                     <PopperWrapper className={cx('menu-popper')}>
                         {history.length > 1 && (
                             <Header
-                                title="Language"
+                                title={current.title}
                                 onBack={() => {
                                     setHistory((prev) => prev.slice(0, prev.length - 1));
                                 }}
@@ -61,5 +61,12 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
         </Tippy>
     );
 }
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
 
 export default Menu;
